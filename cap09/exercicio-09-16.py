feecf3a158ec073ad96cdead988baadc926cf76a
Laender Oliveira <dev.laenderoliveira@gmail.com>
2017-09-22 11:30:09 -0300

    add ex-09-16

---
+++ cap09/exercicio-09-16.py
@@ -0,0 +1,116 @@
+agenda = []
+
+
+def pedenome():
+    return input("Nome: ")
+
+
+def pedetelefone():
+    return input("Telefone: ")
+
+
+def pedearquivo():
+    return input("Nome do arquivo: ")
+
+
+def mostra(nome, telefone):
+    print("Nome: {} Telefone: {}".format(nome, telefone))
+
+
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower() == mnome:
+            return p
+    return None
+
+def novo():
+    nome = pedenome()
+    telefone = pedetelefone()
+    agenda.append([nome, telefone])
+
+
+def apaga():
+    nome = pedenome()
+    p = pesquisa(nome)
+    if p != None:
+        del agenda[p]
+    else:
+        print("Nome não encontrado.")
+
+
+def altera():
+    p = pesquisa(pedenome())
+    if p != None:
+        print("Encontrado!")
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        mostra(nome, telefone)
+        nome = pedenome()
+        telefone = pedetelefone()
+        agenda[p] = [nome, telefone]
+    else:
+        print("Não encontrado")
+
+
+def lista():
+    print("\nAgenda\n")
+    print("-"*6)
+    for nome, telefone in agenda:
+        mostra(nome, telefone)
+    print("-"*6)
+
+
+def grava():
+    nomearquivo = pedearquivo()
+    arquivo = open(nomearquivo, "w")
+    for nome, telefone in agenda:
+        arquivo.write("{}#{}\n".format(nome, telefone))
+    arquivo.close()
+
+
+def le():
+    nomearquivo = pedearquivo()
+    arquivo = open(nomearquivo, "r")
+    for linha in arquivo.readlines():
+        nome, telfone = linha.strip().split("#")
+        agenda.append([nome, telfone])
+    arquivo.close()
+
+def faixa(pergunta, i, f):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if valor >= i and valor <= f:
+                return valor
+        except ValueError:
+            print("Valor inválido, favor digitar valor entre {} e {}".format(i, f))
+
+def menu():
+    print("""
+    0 - Sair
+    1 - Novo
+    2 - Alterar
+    3 - Excluir
+    4 - Lista
+    5 - Grava
+    6 - Lê
+    """)
+    return faixa("Escola uma opção: ", 0, 6)
+
+while True:
+    opcao = menu()
+    if opcao == 0:
+        break
+    elif opcao == 1:
+        novo()
+    elif opcao == 2:
+        altera()
+    elif opcao == 3:
+        apaga()
+    elif opcao == 4:
+        lista()
+    elif opcao == 5:
+        grava()
+    elif opcao == 6:
+        le()
