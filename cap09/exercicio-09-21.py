873fa1497ae3b2b916140a8405bee0e3a64f21fa
Laender Oliveira <dev.laenderoliveira@gmail.com>
2017-09-27 07:17:21 -0300

    add ex-09-21

---
+++ cap09/exercicio-09-21.py
@@ -0,0 +1,142 @@
+agenda = []
+
+
+def pedenome():
+    return input("Nome: ").replace("#", "$")
+
+
+def pedetelefone():
+    return input("Telefone: ").replace("#", "$")
+
+
+def pedearquivo():
+    return input("Nome do arquivo: ")
+
+
+def mostra(nome, telefone):
+    print("Nome: {} Telefone: {}".format(nome, telefone))
+
+
+def pesquisa(nome):
+    mnome = nome.lower()
+    for p, e in enumerate(agenda):
+        if e[0].lower() == mnome:
+            return p
+    return None
+
+def novo():
+    nome = pedenome()
+    telefone = pedetelefone()
+    agenda.append([nome, telefone])
+
+
+def apaga():
+    nome = pedenome()
+    p = pesquisa(nome)
+
+    if p != None:
+        valor = faixa("Tem certeza que quer excluir? (1 - Para confirmar / 0 - para sair): ", 0, 1)
+        if valor == 1:
+            del agenda[p]
+        else:
+            print("Não foi apagado!")
+    else:
+        print("Nome não encontrado.")
+
+
+def altera():
+    p = pesquisa(pedenome())
+    if p != None:
+        print("Encontrado!")
+        nome = agenda[p][0]
+        telefone = agenda[p][1]
+        mostra(nome, telefone)
+        nome = pedenome()
+        telefone = pedetelefone()
+        valor = faixa("Tem certeza que quer alterar? (1 - Para confirmar / 0 - para sair): ", 0, 1)
+        if valor == 1:
+            agenda[p] = [nome, telefone]
+        else:
+            print("Não alterado!")
+    else:
+        print("Não encontrado")
+
+
+def lista():
+    print("\nAgenda\n")
+    print("-"*6)
+    for n, d in enumerate(agenda):
+        nome, telefone = d
+        print(n+1, end=" - ")
+        mostra(nome, telefone)
+    print("-"*6)
+
+
+def grava():
+    nomearquivo = pedearquivo()
+    arquivo = open(nomearquivo, "w")
+    for nome, telefone in agenda:
+        arquivo.write("{}#{}\n".format(nome, telefone))
+    arquivo.close()
+
+
+def le():
+    global agenda
+    agenda = []
+    nomearquivo = pedearquivo()
+    arquivo = open(nomearquivo, "r")
+    for linha in arquivo.readlines():
+        nome, telfone = linha.strip().split("#")
+        agenda.append([nome, telfone])
+    arquivo.close()
+
+
+
+def faixa(pergunta, i, f):
+    while True:
+        try:
+            valor = int(input(pergunta))
+            if valor >= i and valor <= f:
+                return valor
+        except ValueError:
+            print("Valor inválido, favor digitar valor entre {} e {}".format(i, f))
+
+
+def ordena():
+    global agenda
+    agenda.sort()
+    lista()
+
+
+def menu():
+    print("""
+    0 - Sair
+    1 - Novo
+    2 - Alterar
+    3 - Excluir
+    4 - Lista
+    5 - Grava
+    6 - Lê
+    7 - Ordena por Nome
+    """)
+    print("{} contato(s) na agenda.".format(len(agenda)))
+    return faixa("Escola uma opção: ", 0, 7)
+
+while True:
+    opcao = menu()
+    if opcao == 0:
+        break
+    elif opcao == 1:
+        novo()
+    elif opcao == 2:
+        altera()
+    elif opcao == 3:
+        apaga()
+    elif opcao == 4:
+        lista()
+    elif opcao == 5:
+        grava()
+    elif opcao == 6:
+        le()
+    elif opcao == 7:
+        ordena()
