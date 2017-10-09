bed2e83f2e3c40ccef522646890963485ef11215
Laender Oliveira <dev.laenderoliveira@gmail.com>
2017-10-09 10:42:21 -0300

    add listagem09-23

---
+++ cap09/listagem-09-23.py
@@ -0,0 +1,11 @@
+import time
+agora = time.localtime()
+print(f"Ano: {agora.tm_year}")
+print(f"Mês: {agora.tm_mon}")
+print(f"Dia: {agora.tm_mday}")
+print(f"Hora: {agora.tm_hour}")
+print(f"Minuto: {agora.tm_min}")
+print(f"Segundo: {agora.tm_sec}")
+print(f"Dia da Semana: {agora.tm_wday}")
+print(f"Dia do Ano: {agora.tm_yday}")
+print(f"Horário de verão: {agora.tm_isdst}")
