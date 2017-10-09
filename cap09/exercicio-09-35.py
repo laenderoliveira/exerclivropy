edba974f2ad4b9aed480b10ca23e509288ff399c
Laender Oliveira <dev.laenderoliveira@gmail.com>
2017-10-09 12:20:48 -0300

    mod ex09-35

---
+++ cap09/exercicio-09-35.py
@@ -0,0 +1,22 @@
+import os
+import urllib.request
+pagina = open("index.html", "w", encoding="utf-8")
+pagina.write("""<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Arquivos</title>
+</head>
+<body>""")
+
+for raiz, diretorios, arquivos in os.walk("/home/laenderoliveira/Python/livro_nilo/"):
+    pagina.write(f"<h2>{raiz}</h2>")
+    pagina.write("<ul>")
+    for a in arquivos:
+        caminho = os.path.join(raiz, a)
+        link = urllib.request.pathname2url(caminho)
+        tamanho = os.path.getsize(caminho)
+        pagina.write(f"<li><a href={link}>{a} </a>({tamanho} bytes)</li>")
+    pagina.write("</ul>")
+pagina.write("</body></html>")
+pagina.close()
